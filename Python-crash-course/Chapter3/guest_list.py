0cfe0ebe7ceab51c9ed84370d0a53f9da6480f11
Pixelus <s.malafosse@gmail.com>
2019-02-21 11:49:22 +0100

    Create guest_list.py

---
+++ Python-crash-course/Chapter3/guest_list.py
@@ -0,0 +1,5 @@
+guests = ["Karine", "Noëlle", "Kinou", "Kareen"]
+print("Hey " + guests[3] + ", come on to my home for dinner!")
+print(guests[0] + " can you coming home for my birthday today?")
+print("Hi " + guests[1] + " would you like a lunch at home sunday?")
+print(guests[-1] + " come on please to my party...")
