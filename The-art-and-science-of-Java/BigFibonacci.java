3ade31fa427d348f8b0618fb4daf11ae3609ef08
Pixelus <s.malafosse@gmail.com>
2019-02-21 15:47:58 +0100

    Create BigFibonacci.java

---
+++ The-art-and-science-of-Java/BigFibonacci.java
@@ -0,0 +1,20 @@
+// This program displays the value from F(0) through F(9999) for the Fibonacci sequence.
+
+import acm.program.*;
+
+public class BigFibonacci extends ConsoleProgram {
+	public void run() {
+		println("This program displays the value from F(0) through F(9999) for the Fibonacci sequence.");
+		int F0 = 0;
+		int F1 = 1;
+		println("F(0) = " + F0);
+		println("F(1) = " + F1);
+		int F = 0;
+		for(int i = 2; i < 10000; i++) {
+			int F(0) = 0;
+			int F(1)= 1;
+			int value = F(i - 1) + F(i - 2);
+			println(F(" + i + ") = " + value);
+		}
+	}
+}
