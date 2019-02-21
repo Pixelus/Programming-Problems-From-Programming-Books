ae273b24a3aab2a7502448e453cc08ffa5284cee
Pixelus <s.malafosse@gmail.com>
2019-02-21 14:02:00 +0100

    Create restaurant_seating.py

---
+++ Python-crash-course/Chapter7/restaurant_seating.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 29 2018
+@author: PixelNew
+"""
+number = input("How many people are in your dinner group?: ")
+if(int(number) > 8):
+    print("You'll have to wait for a table.")
+else:
+    print("Your table is ready.")
