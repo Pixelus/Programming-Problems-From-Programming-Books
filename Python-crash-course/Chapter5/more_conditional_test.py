37ff5bb53765b48efdefe973b22c46d8b07a2e83
Pixelus <s.malafosse@gmail.com>
2019-02-21 12:00:46 +0100

    Create more_conditional_test.py

---
+++ Python-crash-course/Chapter5/more_conditional_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+
+# Inequality
+print("Is car != 'Toyota'? I predict True.")
+print(car != 'Toyota')
+
+# > < >= <= ...
+number = 5
+print("Number 5 is greather than number 4? I predict True.")
+print(number > 4)
+
+# and or
+print("Number 5 is greather than number 4 and lower than number 6? I predict True.")
+print(number > 4 and number < 6)
+
+# Is in the list?
+list_car = ['audi', 'toyota', 'subaru']
+print("audi is in the list, I predict True.")
+print('audi' in list_car)
+
+# Is not in the list?
+print("jeep isn't in the list, I predict True.")
+print('jeep' not in list_car)
