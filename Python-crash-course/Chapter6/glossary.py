1dc236471342af108abeaafa5ccc2b6c547f956d
Pixelus <s.malafosse@gmail.com>
2019-02-21 12:15:15 +0100

    Create glossary.py

---
+++ Python-crash-course/Chapter6/glossary.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 13 11:00:07 2018
+
+@author: PixelNew
+"""
+
+glossary = {
+    'dictionary': "model an actual dictionary",
+    'loop': "looping through an array, again and again...",
+    'glossary': "name of this dictionary",
+    'Python': "very great language I'm learning",
+    'Key-value': "pair of two associations",
+    }
+
+for i in glossary:
+    print(i + ": " + glossary[i] + "\n")
