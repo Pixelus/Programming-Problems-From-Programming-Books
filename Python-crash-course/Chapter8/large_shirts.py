7e72bdf6c43261671cd7a447fbbfed52a755daf2
Pixelus <s.malafosse@gmail.com>
2019-02-21 14:05:55 +0100

    Create large_shirts.py

---
+++ Python-crash-course/Chapter8/large_shirts.py
@@ -0,0 +1,6 @@
+def make_shirt(size="L", message="I love Python"):
+    print("The size of the T-shirt is " + size + " and the printed text is '" + message + "'.")
+
+make_shirt()
+make_shirt(size="M")
+make_shirt(size="S", message="Love me!")
