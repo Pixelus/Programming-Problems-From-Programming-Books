4ac49ef6a152717d6802d69541094c71020f38b3
Pixelus <s.malafosse@gmail.com>
2019-02-21 14:03:29 +0100

    Create cities.py

---
+++ Python-crash-course/Chapter8/cities.py
@@ -0,0 +1,6 @@
+def describe_city(name, country="Iceland"):
+    print(name.title() + " is in " + country.title() + ".")
+
+describe_city("Paris", country="France")
+describe_city("Reykjavik")
+describe_city("London", country="England")
