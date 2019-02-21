f67002ffe155101014d8262189044a5af18dd04b
Pixelus <s.malafosse@gmail.com>
2019-02-21 11:29:05 +0100

    Create line.c

---
+++ think-like-a-programmer/line.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  line
+//
+//  Created by PixelNew on 14/10/2018.
+//  Copyright © 2018 PixelNew. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int number = 5;
+    for(int i = 0; i < number; i++) {
+        printf("#");
+    }
+    printf("\n");
+    return 0;
+}
