c251cacfa95a7d84cd954a5e03964172fe8b46fe
Pixelus <s.malafosse@gmail.com>
2019-02-21 11:47:40 +0100

    Create greetings.py

---
+++ Python-crash-course/Chapter3/greetings.py
@@ -0,0 +1,5 @@
+names = ["kareen", "karine", "noëlle", "steph"]
+print("Hello " + names[0].title() + ", how are you?")
+print("Hello " + names[1].title() + ", how are you?")
+print("Hello " + names[2].title() + ", how are you?")
+print("Hello " + names[3].title() + ", how are you?")
