6be419194cc7fc6d8f90c11f48452252a754d560
Pixelus <s.malafosse@gmail.com>
2019-02-21 14:06:41 +0100

    Create t_shirt.py

---
+++ Python-crash-course/Chapter8/t_shirt.py
@@ -0,0 +1,5 @@
+def make_shirt(size, text):
+    print("The size of the T-shirt is " + size + " and the printed text is '" + text + "'.")
+
+make_shirt("S", "Only good vibes")
+make_shirt(size="S", text="Only good vibes")
