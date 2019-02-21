3bba706de288761f8074318e1e2dcd123a2995db
Pixelus <s.malafosse@gmail.com>
2019-02-21 12:00:13 +0100

    Create favorite_fruit.py

---
+++ Python-crash-course/Chapter5/favorite_fruit.py
@@ -0,0 +1,11 @@
+favorite_fruits = ["bananas", "apples", "kiwis"]
+if("bananas" in favorite_fruits):
+    print("You really like bananas!")
+if("apples" in favorite_fruits):
+    print("You really like apples!")
+if("cherries" in favorite_fruits):
+    print("You really like cherries!")
+if("ananas" in favorite_fruits):
+    print("You really like ananas!")
+if("kiwis" in favorite_fruits):
+    print("You really like kiwis!")
