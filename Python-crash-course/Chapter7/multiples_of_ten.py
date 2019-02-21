d6f22cbad5e0ac21c7e599f4149110f6216c1b4f
Pixelus <s.malafosse@gmail.com>
2019-02-21 14:00:45 +0100

    Create multiples_of_ten.py

---
+++ Python-crash-course/Chapter7/multiples_of_ten.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 29 2018
+@author: PixelNew
+"""
+number = input("Enter a number please: ")
+if(int(number) % 2 == 0):
+    print("Your number is even.")
+else:
+    print("Your number is odd.")
