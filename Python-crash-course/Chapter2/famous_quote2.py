7f234a4246af4345e85f9d4547fa0a0c26ff732b
Pixelus <s.malafosse@gmail.com>
2019-02-21 11:42:59 +0100

    Create famous_quote2.py

---
+++ Python-crash-course/Chapter2/famous_quote2.py
@@ -0,0 +1,4 @@
+cote = "I have no special talent, I am only passionately curious."
+famous_person = "Albert Einstein"
+message = "\t" + famous_person + " once said, " + '"' + cote + '"'
+print(message)
