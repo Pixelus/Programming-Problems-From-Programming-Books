b9fc4da745e71c687493d65c427ed380b65c65b7
Pixelus <s.malafosse@gmail.com>
2019-02-21 12:17:04 +0100

    Create rivers.py

---
+++ Python-crash-course/Chapter6/rivers.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 13 11:00:07 2018
+
+@author: PixelNew
+"""
+rivers = {
+    'nile': 'egypt',
+    'seine': 'france',
+    'rhone': 'france',
+    }
+
+for key in rivers:
+    print(key + " is in " + rivers[key])
+    print("\n")
+    print(key)
+    print("\n")
+    print(rivers[key])
+    print("\n")
