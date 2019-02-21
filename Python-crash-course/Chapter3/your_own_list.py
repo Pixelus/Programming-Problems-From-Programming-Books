9aa59fcc408a31093aa432a293f021477efa0e1b
Pixelus <s.malafosse@gmail.com>
2019-02-21 11:49:47 +0100

    Create your_own_list.py

---
+++ Python-crash-course/Chapter3/your_own_list.py
@@ -0,0 +1,7 @@
+transportation = ["car", "jet", "bicycle", "snow", "BMX", "skate"]
+print("I would own a " + transportation[0] + " like a camping-car for my travels.")
+print(transportation[1].title() + " is a very fast transportation way to go wherever in the world.")
+print("My " + transportation[2] + " was broken.")
+print("I love to be on my " + transportation[3] + ".")
+print(transportation[-2] + " is so cool and fun on the streets.")
+print("When I was young, I had a " + transportation[-1] + " and it was so funny to play with it!")
