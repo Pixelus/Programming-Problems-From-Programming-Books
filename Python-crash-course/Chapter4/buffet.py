c42cfdf2d84170234d63d4ec774a1d8261cbfa17
Pixelus <s.malafosse@gmail.com>
2019-02-21 11:54:15 +0100

    Create buffet.py

---
+++ Python-crash-course/Chapter4/buffet.py
@@ -0,0 +1,15 @@
+menu = ("vegan", "fruits", "pizza", "burger", "meal")
+
+print("Original menu: ")
+for food in menu:
+    print("- " + food)
+
+# menu[0] = "vegetables"
+# print(menu[0])
+
+# New menu for the buffet
+menu = ("vegetables", "fruits", "pizza", "burger", "pasta")
+print("\n")
+print("Original menu revisited: ")
+for food in menu:
+    print("- " + food)
