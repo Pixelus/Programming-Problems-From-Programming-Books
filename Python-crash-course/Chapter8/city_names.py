cac6bc1325b9a5aced44433810e885b099b3b72d
Pixelus <s.malafosse@gmail.com>
2019-02-21 14:03:46 +0100

    Create city_names.py

---
+++ Python-crash-course/Chapter8/city_names.py
@@ -0,0 +1,6 @@
+def city_country(city_name, country_name):
+    print('"' + city_name.title() + ", " + country_name.title() + '"')
+
+city_country("paris", "france")
+city_country("london", "england")
+city_country("new york", "United States of America")
