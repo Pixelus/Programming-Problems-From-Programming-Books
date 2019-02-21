02deaccaa28644afb32b2a0a5fc09123a0d57e82
Pixelus <s.malafosse@gmail.com>
2019-02-21 13:59:47 +0100

    Create dream_vacation.py

---
+++ Python-crash-course/Chapter7/dream_vacation.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 29 2018
+@author: PixelNew
+"""
+while True:
+    destination = input("If you could visit one place in the world, where would you go? ")
+    if(destination == "quit"):
+        break
+    else:
+        print(destination.title() + " is a very nice place. I'll love to go me too!")
