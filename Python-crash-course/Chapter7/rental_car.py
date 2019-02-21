b6a1f7fa96d786e7547d843d5a7442d789d4609c
Pixelus <s.malafosse@gmail.com>
2019-02-21 14:01:40 +0100

    Create rental_car.py

---
+++ Python-crash-course/Chapter7/rental_car.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 29 2018
+@author: PixelNew
+"""
+rental_car = input("What kind of rental car would you like?: ")
+print("Let me see if I can find a " + rental_car.title() + ".")
