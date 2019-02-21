ea9a06aa3b919aae1ae65a05af6468e8bcc75aa5
Pixelus <s.malafosse@gmail.com>
2019-02-21 11:37:33 +0100

    Create input_validation.py

---
+++ automate-the-boring-stuff-with-Python/input_validation.py
@@ -0,0 +1,14 @@
+def collatz(number):
+    if(number % 2 == 0):
+        print(number // 2)
+        return(number // 2)
+    else: 
+        print(3 * number + 1)   
+        return(3 * number + 1)       
+
+try:
+    user_number = int(input("Enter number: "))
+    while user_number != 1:
+        user_number = collatz(user_number)    
+except ValueError:
+    print("Enter a number please!")
