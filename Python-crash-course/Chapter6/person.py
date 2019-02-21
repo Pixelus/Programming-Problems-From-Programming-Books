5bd9bbb013b7eece3afccce0ed40107ffb52e7fc
Pixelus <s.malafosse@gmail.com>
2019-02-21 12:16:13 +0100

    Create person.py

---
+++ Python-crash-course/Chapter6/person.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 13 11:00:07 2018
+
+@author: PixelNew
+"""
+
+Sandz = {
+    'first_name': "Sandz",
+    'last_name': "Angélibert",
+    'age': 44,
+    'city': "Figeac",
+    }
+
+print(Sandz['first_name'])
+print(Sandz['last_name'])
+print(Sandz['age'])
+print(Sandz['city'])
+print("\n")
+
+for i in Sandz:
+    print(Sandz[i])
