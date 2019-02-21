f1bc42c61d08bee507fff430f49f2a9dc601fe1c
Pixelus <s.malafosse@gmail.com>
2019-02-21 12:14:21 +0100

    Create favorite_numbers.py

---
+++ Python-crash-course/Chapter6/favorite_numbers.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 13 11:00:07 2018
+
+@author: PixelNew
+"""
+favorite_numbers = {
+    'Sandz': 5,
+    'Nan': 6,
+    'Kinou': 9,
+    'Xa': 7,
+    'Max': 8,
+    }
+
+print(favorite_numbers)
+favorite_numbers['Mummy'] = 3
+print(favorite_numbers)
