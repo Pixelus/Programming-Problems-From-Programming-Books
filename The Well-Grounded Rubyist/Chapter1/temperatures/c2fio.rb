62242078553c95647e83c188ea25032f58729e2f
Stéphanie Malafosse <s.malafosse@gmail.com>
2020-07-27 07:45:38 +0200

    Create c2fio.rb

---
+++ The Well-Grounded Rubyist/Chapter1/temperatures/c2fio.rb
@@ -0,0 +1,10 @@
+# Reads a number from a file and writes the Fahrenheit conversion to a different file.
+
+puts "Reading Celsius temperature value from data file..."
+num = File.read("temp.dat")
+celsius = num.to_i
+fahrenheit = (celsius * 9 / 5) + 32
+puts "Saving result to output file 'temp.out'"
+fh = File.new("temp.out", "w")
+fh.puts fahrenheit
+fh.close
